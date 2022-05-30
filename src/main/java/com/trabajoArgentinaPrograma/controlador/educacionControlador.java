85faa10bb24a8c55a2b92869a28601c920757a64
AngelesPiotroski <piotroskiangeles@gmail.com>
2022-05-30 19:52:22 -0300

    cambio de origin

--- src/main/java/com/trabajoArgentinaPrograma/controlador/educacionControlador.java
+++ src/main/java/com/trabajoArgentinaPrograma/controlador/educacionControlador.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1")
-//@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://frontendargentinaprograma.web.app")
 public class educacionControlador {
 
     @Autowired
