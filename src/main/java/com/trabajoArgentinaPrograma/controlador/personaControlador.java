b4cf4add8244e4d91020b4ac68331eb18daa3a1d
AngelesPiotroski <piotroskiangeles@gmail.com>
2022-05-30 17:38:01 -0300

    se agrego la conexion a la bd de clever

--- src/main/java/com/trabajoArgentinaPrograma/controlador/personaControlador.java
+++ src/main/java/com/trabajoArgentinaPrograma/controlador/personaControlador.java
@@ -28,7 +28,8 @@ import org.springframework.web.bind.annotation.PutMapping;
  */
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "http://localhost:4200/")
+//@CrossOrigin(origins = "http://localhost:4200/")
+
 public class personaControlador {
 
     @Autowired
